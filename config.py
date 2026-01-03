1c344491276260921528f61893605093bd7c7cd5
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-03 13:20:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "27433400"
-api_hash = "1a286620de5ffe0a7d9b57e604293555"
-bot_token = ""
-auth_users = [66201066540]
+api_id = "22447622"
+api_hash = "543b62d58d3e723e766ba57a984ab65d"
+bot_token = "8225118430:AAEOFTRcVH3Au1LR0iFyasUb4U5CGATuoT4"
+auth_users = [777756062]
