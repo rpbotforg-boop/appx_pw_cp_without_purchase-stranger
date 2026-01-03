5094e221287137edfbcfef73e046c179376510b2
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-03 13:18:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ image_list = [
 print(4321)
 bot = Client(
     "bot",
-    api_id=27433400,
-    api_hash="1a286620de5ffe0a7d9b57e604293555",
-    bot_token=""
+    api_id=22447622,
+    api_hash="543b62d58d3e723e766ba57a984ab65d",
+    bot_token="8225118430:AAEOFTRcVH3Au1LR0iFyasUb4U5CGATuoT4"
 )
 
 @bot.on_message(filters.command(["start"]))
